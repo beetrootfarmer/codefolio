42afab883247dd6429fce133c02e3f5e9c799b68
hweyoung <gnldud0516@gmail.com>
2022-02-18 23:54:22 +0900

    Email 계정 입력 전

--- src/main/java/com/codefolio/service/EmailService.java
+++ src/main/java/com/codefolio/service/EmailService.java
@@ -1,4 +1,9 @@
 package com.codefolio.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public interface EmailService {
+    public String sendSimpleMessage(String to)throws Exception;
+
 }
