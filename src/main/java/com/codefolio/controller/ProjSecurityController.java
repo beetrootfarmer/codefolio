9b37fc7fe52f1b534b98d95b39c2c6147a539d0c
beetrootfarmer <pos04118@gmail.com>
2022-05-30 22:27:22 +0900

    update projsecurity

--- src/main/java/com/codefolio/controller/ProjSecurityController.java
+++ src/main/java/com/codefolio/controller/ProjSecurityController.java
@@ -5,9 +5,11 @@ import com.codefolio.dto.JsonResponse;
 import com.codefolio.dto.proj.response.GetProjandFileResponse;
 import com.codefolio.dto.proj.response.ProjListResponse;
 import com.codefolio.config.exception.controller.NotFoundException;
+import com.codefolio.config.exception.controller.BadRequestException;
 import com.codefolio.config.exception.controller.GlobalException;
 import com.codefolio.service.FileService;
 import com.codefolio.service.ProjService;
+import com.codefolio.service.UserService;
 import com.codefolio.utils.FileUtils;
 import com.codefolio.vo.Criteria;
 import com.codefolio.vo.FileVO;
@@ -27,8 +29,8 @@ import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RestController
-@RequestMapping("/proj")
-public class ProjController {
+@RequestMapping("/auth")
+public class ProjSecurityController {
 	@Autowired
     ProjService projService;
 	
@@ -38,59 +40,25 @@ public class ProjController {
     @Autowired
     FileUtils fileUtils;
 	private final JwtTokenProvider jwtTokenProvider;
+	private final UserService userService;
 
 
-
-
-	@GetMapping("/search")
-	   public ResponseEntity<?> projList(@RequestParam(required = false) String keyword, ProjVO vo,Criteria cri) {
-			log.info("[API] projList");
-	       
-	       if(vo.getKeywordType() == null) {
-	           vo.setKeywordType("TITLE");
-	       }
-	       if(vo.getKeyword() == null) {
-	           vo.setKeyword("");
-	       }
-	       keyword = vo.getKeyword();
-	      
-	       if(!keyword.equals(vo.getKeyword())) {
-	           cri.setPageNum(1);
-	       }
-	       List<HashMap<String, Object>> projList = projService.getProjList(keyword,cri);
-	
-	        ProjListResponse data = new ProjListResponse(projList);
-	        return ResponseEntity.ok(new JsonResponse(data,"success", 200, "projList"));
-	   }
-
-
-
-	// [프로젝트 상세 페이지] +[파일 불러오기]
-	@GetMapping("/{projSeq}")
-	public ResponseEntity<?> getProjDetail(@PathVariable("projSeq") int seq) {
-		log.info("[API] getProjDetail");
-	   // 보드 시퀀스로 파일리스트 가져오기
-	   List<FileVO> fileList = fileService.getFileListBySeq(seq);
-	
-	   // 조회수 늘리기
-	   projService.viewUp(seq);
-	   ProjVO proj = projService.getProjDetail(seq);
-	
-	   for (FileVO file : fileList) {
-	           if (file.getBoardSeq() == seq) {
-	        	   GetProjandFileResponse data = new GetProjandFileResponse(proj, fileList);
-	    	       return ResponseEntity.ok(new JsonResponse(data,"success", 200, "getProjDetail"));
-	           }
-	       }
-	       GetProjandFileResponse data = new GetProjandFileResponse(proj, fileList);
-	       return ResponseEntity.ok(new JsonResponse(data,"success", 200, "getProjDetail"));
-	}
 
 	
   // [프로젝트 추가] + [파일 추가] ~~~ 썸네일 RequesetParam으로 받아오기  
-  @PostMapping("/add")
-  public ResponseEntity<?> addProj(@RequestParam(required = false) MultipartFile tn, @RequestParam(required = false) MultipartFile[] file,ProjVO vo) throws IOException  {
+  @PostMapping("/{userId}/add")
+  public ResponseEntity<?> addProj(@PathVariable String userId,
+  									@RequestParam(required = false) MultipartFile tn, 
+  									@RequestParam(required = false) MultipartFile[] file,
+									ProjVO vo, 
+									HttpServletRequest request) 
+									throws IOException  {
 	  		log.info("[API] addProj");
+
+			String userUUID = getUUID(request);
+			String getuserUUID = userService.getUUIDById(userId);
+			if(!getuserUUID.equals(userUUID)) throw new BadRequestException("user url error");
+
 	  		try{
  	 			if(!tn.isEmpty()) {
      	 		String thumbnail = projService.makeThumbnail(tn);
@@ -115,7 +83,6 @@ public class ProjController {
  }
 
 
-
 // [프로젝트 삭제]
         @DeleteMapping("/{projSeq}")
         public ResponseEntity<?> deleteProj(@PathVariable("projSeq") int projSeq) {
@@ -131,11 +98,19 @@ public class ProjController {
 
 
 // [프로젝트 수정]  + [파일 수정]
-		@PutMapping("update/{projSeq}")
-		public ResponseEntity<?> updateProj(@PathVariable("projSeq") int projSeq,@RequestParam(required = false) MultipartFile tn, 
-											@RequestParam(required = false) MultipartFile[] file,
+		@PutMapping("/{userId}/update/{projSeq}")
+		public ResponseEntity<?> updateProj(@PathVariable String userId,
+											@PathVariable("projSeq") int projSeq,
+											@RequestParam(required = false) MultipartFile tn, 
+											@RequestParam(required = false) MultipartFile[] file, 
+											HttpServletRequest request,
 		                                    ProjVO vo) {
 			log.info("[API] updateProj");
+
+			String userUUID = getUUID(request);
+			String getuserUUID = userService.getUUIDById(userId);
+			if(!getuserUUID.equals(userUUID)) throw new BadRequestException("user url error");
+
 			try {
 			
 				if(!tn.isEmpty()) {
@@ -170,8 +145,12 @@ public class ProjController {
 		
 // 유저별 좋아요한 프로젝트 조회
 		@GetMapping("likeProj/{userId}")
-		public ResponseEntity<?> getLikeProj(@PathVariable("userId") String userId, ProjVO vo,Criteria cri) {
+		public ResponseEntity<?> getLikeProj(@PathVariable String userId, ProjVO vo,Criteria cri, HttpServletRequest request) {		
 		log.info("[API] getLikeProj");
+
+		String userUUID = getUUID(request);
+        String getuserUUID = userService.getUUIDById(userId);
+        if(!getuserUUID.equals(userUUID)) throw new BadRequestException("user url error");
 		
 		cri.setPageNum(1);
 		List<HashMap<String, Object>> projList = projService.getLikeProj(userId,cri);
@@ -179,15 +158,16 @@ public class ProjController {
         return ResponseEntity.ok(new JsonResponse(data, "success",200, "likeProj"));
 		}
 		
-// 좋아요 순 프로젝트 리스트
-		@GetMapping("/bestProj") 
-		public ResponseEntity<?> getbestProj(ProjVO vo,Criteria criteria) {
-		log.info("[API] getbestProj");
-		
-		criteria.setPageNum(1);
-		List<HashMap<String, Object>> projList = projService.getBestProj(criteria);
-		ProjListResponse data = new ProjListResponse(projList);
-        return ResponseEntity.ok(new JsonResponse(data,"success", 200, "bestProj"));
+		private String getUUID(HttpServletRequest request){
+			try{
+				String getAcToken = jwtTokenProvider.resolveToken(request);
+				return jwtTokenProvider.getUserPk(getAcToken);
+			}catch (NullPointerException e){
+				throw new NotFoundException("not Resolve AcToken");
+	
+			} catch (Exception e){
+				throw new GlobalException("global Exception");
+			}
 		}
 
 }
